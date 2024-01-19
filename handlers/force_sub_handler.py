c2133b870508ebea2d80d2f72e64c2002ee205f4
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2024-01-20 01:00:42 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -1,12 +1,9 @@
 import asyncio
-from typing import (
-    Union
-)
+from typing import Union
 from configs import Config
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
 
 async def get_invite_link(bot: Client, chat_id: Union[str, int]):
     try:
@@ -17,50 +14,53 @@ async def get_invite_link(bot: Client, chat_id: Union[str, int]):
         await asyncio.sleep(e.value)
         return await get_invite_link(bot, chat_id)
 
-
 async def handle_force_sub(bot: Client, cmd: Message):
-    if Config.UPDATES_CHANNEL and Config.UPDATES_CHANNEL.startswith("-100"):
-        channel_chat_id = int(Config.UPDATES_CHANNEL)
-    elif Config.UPDATES_CHANNEL and (not Config.UPDATES_CHANNEL.startswith("-100")):
-        channel_chat_id = Config.UPDATES_CHANNEL
-    else:
+    if not Config.FORCE_SUB_CHANNELS:
         return 200
-    try:
-        user = await bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
-        if user.status == "kicked":
-            await bot.send_message(
-                chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion).",
-                disable_web_page_preview=True
-            )
-            return 400
-    except UserNotParticipant:
+
+    user_id = cmd.from_user.id
+
+    buttons = []
+    for channel_id in Config.FORCE_SUB_CHANNELS:
         try:
-            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
-        except Exception as err:
-            print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
-            return 200
-        await bot.send_message(
-            chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\n"
-                 "Due to Overload, Only Channel Subscribers can use this Bot!",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
-                    ],
-                    [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshForceSub")
-                    ]
-                ]
+            channel_info = await bot.get_chat(channel_id)
+            channel_name = channel_info.title
+            user = await bot.get_chat_member(chat_id=channel_id, user_id=user_id)
+            if user.status == "kicked":
+                await bot.send_message(
+                    chat_id=user_id,
+                    text="Sorry Sir, You are Banned to use me. Contact @Blade_9".",
+                    disable_web_page_preview=True
+                )
+                return 400
+        except UserNotParticipant:
+            try:
+                invite_link = await get_invite_link(bot, chat_id=channel_id)
+            except Exception as err:
+                print(f"Unable to do Force Subscribe to {channel_id}\n\nError: {err}")
+                return 200
+
+            buttons.append(
+                [InlineKeyboardButton(f"{channel_name}", url=invite_link.invite_link)]
             )
+    
+    if not buttons:
+        # User is a participant in all channels, return 200 (success)
+        return 200
+
+    try:
+        await bot.send_message(
+            chat_id=user_id,
+            text="**Please Join My Updates Channel(s) to use this Bot!**\n\n"
+                 "Due to Overload, Only Channel Subscribers can use this Bot!",
+            reply_markup=InlineKeyboardMarkup(buttons + [[InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshForceSub")]])
         )
-        return 400
     except Exception:
         await bot.send_message(
-            chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion).",
+            chat_id=user_id,
+            text="Something went Wrong. Contact @Blade_9",
             disable_web_page_preview=True
         )
         return 200
-    return 200
+
+    return 400
