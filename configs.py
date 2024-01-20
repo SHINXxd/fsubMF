cf70b4a20bf58174edc342e2c49b9aedadb5694e
SHINXxd <151854453+SHINXxd@users.noreply.github.com>
2024-01-20 17:29:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,18 +1,18 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  FORCE_SUB_CHANNELS = []
+  API_ID = int(os.environ.get("API_ID", "27570599"))
+  API_HASH = os.environ.get("API_HASH", "0329a786739cb73a2b1176582c5e8376")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6847882710:AAHDlPscai2YcU_TakwduvljH35IbX2uXTs")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "@JirayaSage_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001829601382"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "Nono")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "None")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "5468078784"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://lodukuwaraboy:Qwerty12@cluster0.mfey0ob.mongodb.net/?retryWrites=true&w=majority")
+  FORCE_SUB_CHANNELS = [-1002043494376, -1001811788351, -1002132279172, -1002039598333]
   #FORCE_SUB_CHANNELS =[int(x) for x in os.environ.get("UPDATES_CHANNEL", "").split(",") if x]
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001991863694"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
